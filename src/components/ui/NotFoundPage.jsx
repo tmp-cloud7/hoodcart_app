de644e5bb00593b447c445d6d55f62e07ee96996
taio-ops <tayopopoola92@gmail.com>
2025-10-16 18:19:23 +0100

    Modified NotFoundPage.module.css

--- src/components/ui/NotFoundPage.jsx
+++ src/components/ui/NotFoundPage.jsx
@@ -6,8 +6,12 @@ const NotFoundPage = () => {
   return (
     <div className={`d-flex flex-column justify-content-center align-items-center vh-100 text-center ${styles.wrapper}`}>
       <h1 className={`fw-bold display-1 mb-3 ${styles.code}`}>404</h1>
-      <h2 className="fw-semibold mb-2">Oops! Page Not Found</h2>
-      <p className="text-muted mb-4">
+      <h2 style={{   
+            color: "white",       
+          }}className="fw-semibold mb-2">Oops! Page Not Found</h2>
+      <p style={{   
+            color: "white",       
+          }}className="text-muted mb-4">
         The page you’re looking for doesn’t exist or has been moved.
       </p>
       <Link to="/" className={`btn btn-dark rounded-pill px-4 py-2 ${styles.homeBtn}`}>
